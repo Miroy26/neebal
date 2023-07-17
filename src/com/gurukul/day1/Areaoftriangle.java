811bf4ed1d8438021368203de49adb6556a402e8
Miroy26 <miroyparmar@gmail.com>
2023-07-17 17:26:16 +0530

    2nd revision

--- src/com/gurukul/day1/Areaoftriangle.java
+++ src/com/gurukul/day1/Areaoftriangle.java
@@ -5,7 +5,7 @@ public class Areaoftriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Enter the value of the side");
+System.out.println("Enter the value of the side");//My name is Miroy Parmar
 Scanner sc=new Scanner(System.in);
 int s= sc.nextInt();
 double Area= (Math.sqrt(3)/4)*Math.pow(s, 2);
